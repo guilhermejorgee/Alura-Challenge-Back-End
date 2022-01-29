a7f2044734ba2d7207834a55e21c154d7f073c55
Guilherme Jorge <guilhermejorgee@gmail.com>
2022-01-28 23:14:15 -0300

    .

--- src/main/java/br/com/alura/challenge/backend/BackendApplication.java
+++ src/main/java/br/com/alura/challenge/backend/BackendApplication.java
@@ -1,11 +1,7 @@
 package br.com.alura.challenge.backend;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
 
-
-@SpringBootApplication(exclude = {GsonAutoConfiguration.class})
 public class BackendApplication {
 
 	public static void main(String[] args) {
